6690ae7cc5adc6478ae9c5ae0df5a78ce1164b82
Seinxera02 <seinxera12@gmail.com>
2026-05-29 17:26:10 +0545

    chore(deps): add jsqr and vite-plugin-mkcert, enable HTTPS and network host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
+    host: true,
     proxy: {
       '/map':    'http://localhost:8000',
       '/route':  'http://localhost:8000',
